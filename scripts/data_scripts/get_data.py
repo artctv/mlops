17bdf311ff573cbaaacc66eecdd68db798ec349f
Artem 'artctv <artctvcode@gmail.com>
2023-12-18 13:25:44 +0500

    reinit project for lab 4

--- scripts/data_scripts/get_data.py
+++
@@ -1,6 +0,0 @@
-from catboost.datasets import titanic
-
-train, test = titanic()
-
-train.to_csv("/root/mlops/data/raw/train.csv", columns=train.columns)
-test.to_csv("/root/mlops/data/raw/test.csv", columns=test.columns)
